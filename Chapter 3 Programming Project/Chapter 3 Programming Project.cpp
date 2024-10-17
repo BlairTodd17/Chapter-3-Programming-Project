7d7756a43de9f8f9bd68f71db1faea2f03f6db62
Blair Todd <bjbbt4@outlook.com>
2024-10-16 22:12:14 -0400

    q

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
+#include <cstdlib>  
+#include <ctime>   
 using namespace std;
 
 int main() {
-    const double SUGAR = 1.5;   
-    const double BUTTER = 1.0; 
-    const double FLOUR = 2.75; 
-    const int COOKIES = 48;    
+    
+    srand(static_cast<unsigned int>(time(0)));
 
-    int cookiesWanted;
-    cout << "How many cookies do you want to make? ";
-    cin >> cookiesWanted;
+    int num1 = rand() % 100 + 1; 
+    int num2 = rand() % 100 + 1;  
 
-    double sugarNeeded = (SUGAR / COOKIES) * cookiesWanted;
-    double butterNeeded = (BUTTER / COOKIES) * cookiesWanted;
-    double flourNeeded = (FLOUR / COOKIES) * cookiesWanted;
+    cout << "Solve the following problem:\n";
+    cout << num1 << " + " << num2 << " = ?\n";
 
-    cout << "For " << cookiesWanted << " cookies, you will need:\n";
-    cout << sugarNeeded << " cups of sugar\n";
-    cout << butterNeeded << " cups of butter\n";
-    cout << flourNeeded << " cups of flour\n";
+
+    cout << "Press Enter when you're ready for the solution...";
+    cin.get(); 
+
+    int correctAnswer = num1 + num2;
+    cout << "The correct answer is: " << correctAnswer << endl;
 
     return 0;
 }
